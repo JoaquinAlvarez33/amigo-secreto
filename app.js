4755ecfe4f8377e5715681552c390c85123abb7e
Joaquin Alvarez <medinaalvarezjoaquin@gmail.com>
2025-02-27 12:25:12 -0600

    Funcion agregar  Amigo

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+document.addEventListener('DOMContentLoaded', function () {
+    let amigos = [];
+
+    function agregarAmigo() {
+        let nombre = document.getElementById('amigo').value; // Asegúrate de que el id en el input sea 'amigo'
+        if (nombre === '') {
+            alert('Debe ingresar un nombre');
+            return;
+        } else {
+            amigos.push(nombre);
+            document.getElementById('amigo').value = ''; // Limpia el campo de entrada
+            console.log(amigos);
+        }
+    }
+
+    function sortearAmigo() {
+       
+    }
+    
+    // Aquí asignamos el evento del botón 'Añadir' a la función
+    document.querySelector('.button-add').addEventListener('click', agregarAmigo);
+    // Aquí asignamos el evento del botón 'Sortear amigo' a la función
+    document.querySelector('.button-draw').addEventListener('click', sortearAmigo);
+});
